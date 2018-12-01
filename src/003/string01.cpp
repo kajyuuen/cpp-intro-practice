0298488a68e6d0a30db88659eb6866ac3c635c3c
kajyuuen <kajyuuen@gmail.com>
2018-12-01 21:23:51 +0900

    Finish ch03

---
+++ src/003/string01.cpp
@@ -0,0 +1,5 @@
+int main()
+{
+    std::cout << "hello"s ;
+    std::cout << "hello" ;
+}
---
+++ src/003/variable02.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+    auto x
+    = 123 ;
+
+    std::cout << x ;
+
+    x = 456 ;
+
+    std::cout << x ;
+
+    x = 789 ;
+    std::cout << x ;
+}
